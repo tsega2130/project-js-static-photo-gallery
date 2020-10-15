ef95d8c6b50064839fc227e731065c95f93ac9db
Natalie Anderson <natalie.anderson6@gmail.com>
2020-10-15 16:00:14 -0700

    Made the fs.writeFileSync method work, which created new index.html file

--- gallery.js
+++ gallery.js
@@ -4,10 +4,22 @@ let imageFileNames = fs.readdirSync('site/images');
 
 // This code is just here to demonstrate how fs.readdirSync works
 
-console.log('sites/images contains the following files:');
+// console.log('sites/images contains the following files:');
+
+let data = '';
 
 for (let i = 0; i < imageFileNames.length; i++) {
   let fileName = imageFileNames[i];
+  // console.log(`${i + 1}. ${fileName}`);
+  console.log(`<img src="${fileName}" alt="${fileName}" class="${fileName}">`);
 
-  console.log(`${i + 1}. ${fileName}`);
+  data += '<img src="';
+  data += `${fileName}" `;
+  data += 'alt="';
+  data += `${fileName}" `;
+  data += 'class="';
+  data += `${fileName}">`;
+  data += '\n';
 }
+
+fs.writeFileSync('site/index.html', data);
