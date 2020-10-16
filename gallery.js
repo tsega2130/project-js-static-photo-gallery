5cbfe128093fb492c5d7cab8bf297c38e2835409
tsega2130 <69941454+tsega2130@users.noreply.github.com>
2020-10-16 15:34:28 -0400

    final product

--- gallery.js
+++ gallery.js
@@ -1,25 +1,302 @@
 let fs = require('fs');
 
-let imageFileNames = fs.readdirSync('site/images');
+//let imageFileNames = fs.readdirSync('site/images');
 
-// This code is just here to demonstrate how fs.readdirSync works
-
-// console.log('sites/images contains the following files:');
 
 let data = '';
+let keyword=["Afghanistan",
+"Albania",
+"Algeria",
+"American Samoa",
+"Andorra",
+"Angola",
+"Anguilla",
+"Antarctica",
+"Antigua and Barbuda",
+"Argentina",
+"Armenia",
+"Aruba",
+"Australia",
+"Austria",
+"Azerbaijan",
+"Bahamas",
+"Bahrain",
+"Bangladesh",
+"Barbados",
+"Belarus",
+"Belgium",
+"Belize",
+"Benin",
+"Bermuda",
+"Bhutan",
+"Bolivia",
+"Bonaire, Sint Eustatius and Saba",
+"Bosnia and Herzegovina",
+"Botswana",
+"Bouvet Island",
+"Brazil",
+"British Indian Ocean Territory",
+"Brunei Darussalam",
+"Bulgaria",
+"Burkina Faso",
+"Burundi",
+"Cabo Verde",
+"Cambodia",
+"Cameroon",
+"Canada",
+"Cayman Islands",
+"Central African Republic",
+"Chad",
+"Chile",
+"China",
+"Christmas Island",
+"Cocos (Keeling) Islands",
+"Colombia",
+"Comoros",
+"The Democratic Republic of the Congo",
+"The Congo",
+"Cook Islands",
+"Costa Rica",
+"Croatia",
+"Cuba",
+"Curaçao",
+"Cyprus",
+"Czechia",
+"Côte d'Ivoire",
+"Denmark",
+"Djibouti",
+"Dominica",
+"Dominican Republic",
+"Ecuador",
+"Egypt",
+"El Salvador",
+"Equatorial Guinea",
+"Eritrea",
+"Estonia",
+"Eswatini",
+"Ethiopia",
+"Falkland Islands",
+"Faroe Islands",
+"Fiji",
+"Finland",
+"France",
+"French Guiana",
+"French Polynesia",
+"French Southern Territories",
+"Gabon",
+"Gambia",
+"Georgia",
+"Germany",
+"Ghana",
+"Gibraltar",
+"Greece",
+"Greenland",
+"Grenada",
+"Guadeloupe",
+"Guam",
+"Guatemala",
+"Guernsey",
+"Guinea",
+"Guinea-Bissau",
+"Guyana",
+"Haiti",
+"Heard Island and McDonald Islands",
+"Holy See",
+"Honduras",
+"Hong Kong",
+"Hungary",
+"Iceland",
+"India",
+"Indonesia",
+"Iran",
+"Iraq",
+"Ireland",
+"Isle of Man",
+"Israel",
+"Italy",
+"Jamaica",
+"Japan",
+"Jersey",
+"Jordan",
+"Kazakhstan",
+"Kenya",
+"Kiribati",
+"The DPRK",
+"South Korea",
+"Kuwait",
+"Kyrgyzstan",
+"Laos",
+"Latvia",
+"Lebanon",
+"Lesotho",
+"Liberia",
+"Libya",
+"Liechtenstein",
+"Lithuania",
+"Luxembourg",
+"Macao",
+"Madagascar",
+"Malawi",
+"Malaysia",
+"Maldives",
+"Mali",
+"Malta",
+"Marshall Islands",
+"Martinique",
+"Mauritania",
+"Mauritius",
+"Mayotte",
+"Mexico",
+"Micronesia",
+"Moldova",
+"Monaco",
+"Mongolia",
+"Montenegro",
+"Montserrat",
+"Morocco",
+"Mozambique",
+"Myanmar",
+"Namibia",
+"Nauru",
+"Nepal",
+"Netherlands",
+"New Caledonia",
+"New Zealand",
+"Nicaragua",
+"Niger",
+"Nigeria",
+"Niue",
+"Norfolk Island",
+"Northern Mariana Islands",
+"Norway",
+"Oman",
+"Pakistan",
+"Palau",
+"Palestine",
+"Panama",
+"Papua New Guinea",
+"Paraguay",
+"Peru",
+"Philippines",
+"Pitcairn",
+"Poland",
+"Portugal",
+"Puerto Rico",
+"Qatar",
+"Republic of North Macedonia",
+"Romania",
+"Russian Federation",
+"Rwanda",
+"Réunion",
+"Saint Barthélemy",
+"Saint Helena, Ascension and Tristan da Cunha",
+"Saint Kitts and Nevis",
+"Saint Lucia",
+"Saint Martin",
+"Saint Pierre and Miquelon",
+"Saint Vincent and the Grenadines",
+"Samoa",
+"San Marino",
+"Sao Tome and Principe",
+"Saudi Arabia",
+"Senegal",
+"Serbia",
+"Seychelles",
+"Sierra Leone",
+"Singapore",
+"Sint Maarten",
+"Slovakia",
+"Slovenia",
+"Solomon Islands",
+"Somalia",
+"South Africa",
+"South Georgia and the South Sandwich Islands",
+"South Sudan",
+"Spain",
+"Sri Lanka",
+"Sudan",
+"Suriname",
+"Svalbard and Jan Mayen",
+"Sweden",
+"Switzerland",
+"Syrian Arab Republic",
+"Taiwan",
+"Tajikistan",
+"Tanzania, United Republic of",
+"Thailand",
+"Timor-Leste",
+"Togo",
+"Tokelau",
+"Tonga",
+"Trinidad and Tobago",
+"Tunisia",
+"Turkey",
+"Turkmenistan",
+"Turks and Caicos Islands",
+"Tuvalu",
+"Uganda",
+"Ukraine",
+"United Arab Emirates",
+"United Kingdom of Great Britain and Northern Ireland",
+"United States Minor Outlying Islands",
+"United States of America",
+"Uruguay",
+"Uzbekistan",
+"Vanuatu",
+"Venezuela",
+"Viet Nam",
+"Virgin Islands (British)",
+"Virgin Islands (U.S.)",
+"Wallis and Futuna",
+"Western Sahara",
+"Yemen",
+"Zambia",
+"Zimbabwe",
+"Åland Islands"];
 
-for (let i = 0; i < imageFileNames.length; i++) {
-  let fileName = imageFileNames[i];
-  // console.log(`${i + 1}. ${fileName}`);
-  console.log(`<img src="${fileName}" alt="${fileName}" class="${fileName}">`);
 
-  data += '<img src="';
-  data += `${fileName}" `;
-  data += 'alt="';
-  data += `${fileName}" `;
-  data += 'class="';
-  data += `${fileName}">`;
-  data += '\n';
-}
 
-fs.writeFileSync('site/index.html', data);
+for (let i = 0; i < 20; i++) {
+  let randomKeyword = Math.floor((Math.random()*keyword.length))
+  console.log(keyword[randomKeyword])
+    let country=keyword[randomKeyword];
+    let fileName = `http://source.unsplash.com/1600x900/?${country}`;
+
+    data += `<div class="gallery-container"> \n \t <div class="gallery-item"> \n \t  <div class="image"> \n \t <img src="`;
+  data += `${fileName}" > \n \t  </div> \n \t </div> \n \t </div> \n`;
+  if (fileName === "https://images.unsplash.com/source-404?fit=crop&fm=jpg&h=800&q=60&w=1200") {
+    let randomKeyword = Math.floor((Math.random()*keyword.length))
+  console.log(keyword[randomKeyword])
+    let country=keyword[randomKeyword];
+    let fileName = `http://source.unsplash.com/1600x900/?${country}`;
+  }
+
+  }
+
+
+let fullPage= `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Photo Gallery</title>
+  <link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="main.css">
+  <link href="https://fonts.googleapis.com/css2?family=Karla:wght@700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,700;1,400&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="textContainer">
+  <h1>travel moodboard</h1>
+</div>
+<div class ="container"> 
+      ${data}
+</div>
+</body>
+</html>
+`;
+
+
+fs.writeFileSync('site/index.html', fullPage);
+
